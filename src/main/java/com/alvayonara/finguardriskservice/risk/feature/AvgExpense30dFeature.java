2eed57d1cb4e10662e436fb32ef45346940ecb61
Alva Yonara Puramandya <alvayonara@outlook.com>
2026-02-09 00:01:36 +0700

    impl feature (month summary + avg expense 30d)

---
+++ src/main/java/com/alvayonara/finguardriskservice/risk/feature/AvgExpense30dFeature.java
@@ -0,0 +1,32 @@
+package com.alvayonara.finguardriskservice.risk.feature;
+
+
+import com.alvayonara.finguardriskservice.risk.constants.FeatureConstants;
+import com.alvayonara.finguardriskservice.risk.engine.RiskContext;
+import com.alvayonara.finguardriskservice.transaction.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Component
+public class AvgExpense30dFeature implements RiskFeature {
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Override
+    public String name() {
+        return FeatureConstants.AVG_EXPENSE_30D;
+    }
+
+    @Override
+    public Mono<Void> compute(RiskContext context) {
+        return transactionRepository.findAvgExpenseLast30Days(context.getUserId())
+                .doOnNext(avg ->
+                        context.getFeatures().put(name(), Objects.isNull(avg) ? BigDecimal.ZERO : avg)
+                )
+                .then();
+    }
+}
---
+++ src/main/java/com/alvayonara/finguardriskservice/risk/feature/MonthlySummaryFeature.java
@@ -0,0 +1,27 @@
+package com.alvayonara.finguardriskservice.risk.feature;
+
+import com.alvayonara.finguardriskservice.risk.constants.FeatureConstants;
+import com.alvayonara.finguardriskservice.risk.engine.RiskContext;
+import com.alvayonara.finguardriskservice.summary.MonthlySummaryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class MonthlySummaryFeature implements RiskFeature {
+    @Autowired
+    private MonthlySummaryRepository monthlySummaryRepository;
+
+    @Override
+    public String name() {
+        return FeatureConstants.MONTHLY_SUMMARY;
+    }
+
+    @Override
+    public Mono<Void> compute(RiskContext context) {
+        return monthlySummaryRepository
+                .findByUserIdAndMonthKey(context.getUserId(), context.getMonthKey())
+                .doOnNext(summary -> context.getFeatures().put(name(), summary))
+                .then();
+    }
+}
