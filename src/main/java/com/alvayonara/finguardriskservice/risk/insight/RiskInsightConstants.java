6c5b6a815ce0d94cd0bc7ccf65ed62d4716e6824
Alva Yonara Puramandya <alvayonara@outlook.com>
2026-02-09 06:21:43 +0700

    impl insight base on risk signal

---
+++ src/main/java/com/alvayonara/finguardriskservice/risk/insight/RiskInsightConstants.java
@@ -0,0 +1,8 @@
+package com.alvayonara.finguardriskservice.risk.insight;
+
+
+public final class RiskInsightConstants {
+    public static final String MSG_NEGATIVE_CASH_FLOW = "Pengeluaran bulan ini lebih besar dari pemasukan.";
+    public static final String MSG_EXPENSE_SPIKE = "Pengeluaran kamu jauh lebih besar dari rata-rata biasanya.";
+    public static final String MSG_GENERIC = "Terdeteksi aktivitas keuangan yang tidak biasa.";
+}
