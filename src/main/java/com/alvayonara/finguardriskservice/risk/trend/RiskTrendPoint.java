d04fdca9d8d653cd2b16f2c056b5bc3b50a6b5e3
Alva Yonara Puramandya <alvayonara@outlook.com>
2026-02-10 08:07:23 +0700

    implement risk trend
    used for FE chart

---
+++ src/main/java/com/alvayonara/finguardriskservice/risk/trend/RiskTrendPoint.java
@@ -0,0 +1,12 @@
+package com.alvayonara.finguardriskservice.risk.trend;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class RiskTrendPoint {
+    private String date;
+    private String level;
+    private int score;
+}
