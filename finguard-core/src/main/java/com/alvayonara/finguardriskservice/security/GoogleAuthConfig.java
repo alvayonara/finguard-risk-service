55acebc80c5b7ea2900b230edf29fb7de58ca976
Alva Yonara Puramandya <alvayonara@outlook.com>
2026-02-18 06:11:23 +0700

    adjust google client id

--- finguard-core/src/main/java/com/alvayonara/finguardriskservice/security/GoogleAuthConfig.java
+++ finguard-core/src/main/java/com/alvayonara/finguardriskservice/security/GoogleAuthConfig.java
@@ -3,7 +3,10 @@ package com.alvayonara.finguardriskservice.security;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
+
 import java.util.Collections;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +14,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GoogleAuthConfig {
 
-  @Bean
-  public GoogleIdTokenVerifier googleIdTokenVerifier(
-      @Value("${google.client-id}") String clientId) {
-    return new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
-        .setAudience(Collections.singletonList(clientId))
-        .build();
-  }
+    @Bean
+    public GoogleIdTokenVerifier googleIdTokenVerifier(
+            @Value("${google.android.client-id}") String androidClientId,
+            @Value("${google.ios.client-id}") String iosClientId) {
+        return new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
+                .setAudience(List.of(androidClientId, iosClientId))
+                .build();
+    }
 }
